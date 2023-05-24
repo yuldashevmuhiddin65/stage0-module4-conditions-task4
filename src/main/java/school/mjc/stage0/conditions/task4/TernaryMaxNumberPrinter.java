25d5b982e5b816e942363fa9ce58e98f9082bfa8
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-24 16:27:55 +0500

    I`ve solved this exercise!

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > second && first > third ? first : second > first && second > third ? second : third;
+        System.out.println(result);
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String result = first > second ? "first" : "second";
+        System.out.println(result);
     }
 }
